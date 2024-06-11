1d0966ae8046d05dbfab86768aa05fc778bb1cbe
Francisco Lopez <lopezf28@gmail.com>
2024-06-10 23:52:15 -0400

    generando componente Login

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <input v-model="email" type="email" placeholder="Email" />
+            <input v-model="password" type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { auth } from "../firebase";
+
+export default {
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        async login() {
+            try {
+                await auth.signInWithEmailAndPassword(this.email, this.password);
+                this.$router.push("/home");
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    },
+};
+</script>
